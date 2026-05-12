c319e35cf0bf85bc7d882a0a24d37bebe48b47b3
Anders Assev-Kvalvaag <anders.assev-kvalvaag@bouvet.no>
2026-05-12 15:05:53 +0200

    End of section 19

---
+++ Application/Activities/Commands/AddComment.cs
@@ -0,0 +1,49 @@
+﻿using Application.Activities.DTOs;
+using Application.Core;
+using Application.Interfaces;
+using AutoMapper;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Activities.Commands;
+
+public class AddComment
+{
+    public class Command : IRequest<Result<CommentDTO>>
+    {
+        public required string Body { get; set; }
+        public required string ActivityId { get; set; }
+    }
+
+    public class Handler(AppDbContext context, IMapper mapper, IUserAccessor userAccessor)
+        : IRequestHandler<Command, Result<CommentDTO>>
+    {
+        public async Task<Result<CommentDTO>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var activity = await context.Activities
+                .Include(x => x.Comments)
+                .ThenInclude(x => x.User)
+                .FirstOrDefaultAsync(x => x.Id == request.ActivityId, cancellationToken);
+
+            if (activity == null) return Result<CommentDTO>.Failure("Could not find activity", 404);
+            
+            var user = await userAccessor.GetUserAsync();
+
+            var comment = new Comment
+            {
+                Body = request.Body,
+                ActivityId = request.ActivityId,
+                UserId = user.Id
+            };
+            
+            activity.Comments.Add(comment);
+            
+            var result = await context.SaveChangesAsync(cancellationToken) > 0;
+            return result ? 
+                Result<CommentDTO>.Success(mapper.Map<CommentDTO>(comment)) 
+                : Result<CommentDTO>.Failure("Failed to add comment", 400);
+        }
+    }
+}
